1060c77bb7ebfe5d78c4d6c3e5a99667b1c4e2c8
Encho <38841444+Enchoseon@users.noreply.github.com>
2022-02-06 13:05:26 -0800

    Update gui.js
    Moved more functionality into the packetHandler, improved comments, and worsened inter-module dependencies.

--- util/gui.js
+++ util/gui.js
@@ -2,7 +2,12 @@
 // Imports
 // =======
 
+const fs = require("fs");
+
 const logger = require("./logger.js");
+const notifier = require("./notifier.js");
+
+const config = JSON.parse(fs.readFileSync("config.json"));
 
 // ===========
 // Global Vars
@@ -14,7 +19,7 @@ var data = {
 	"eta": "CHECKING...",
 	"restart": "None",
 	"mineflayer": "CHECKING...",
-	"inQueue": "CHECKING..."
+	"inQueue": false
 };
 var dataDefault = data;
 
@@ -22,7 +27,11 @@ var dataDefault = data;
 // Functions
 // =========
 
-// Display a basic GUI
+/**
+ * Display a basic CLI GUI
+ * @param {string} type
+ * @param {string} input
+ */
 function display(type, input) {
 	if (eval("data." + type + " !== `" + input + "`")) { // Jankus maximus
 		eval("data." + type + " = `" + input + "`");
@@ -36,20 +45,73 @@ function display(type, input) {
 88888 888888 8   8 88888 8888 88888 88888 8888888 8   8 8   8 
 		`);
 		console.log("\x1b[30m", "Enchoseon#1821 was here!");
-		console.log("\x1b[33m", "Last Update: [" + logger.getTimestamp(":") + "]");
+		console.log("\x1b[33m", "Last Update: [" + logger.getTimestamp() + "]");
 		console.log("\x1b[33m", "Current Queue Position: " + data.position);
 		console.log("\x1b[33m", "ETA: " + data.eta);
 		console.log("\x1b[33m", "Restart: " + data.restart);
 		console.log("\x1b[35m", "Mineflayer Running: " + data.mineflayer.toUpperCase());
-		console.log("\x1b[35m", "In Queue Server: " + data.inQueue.toUpperCase());
+		console.log("\x1b[35m", "In Queue Server: " + data.inQueue.toString().toUpperCase());
 
 		logger.log("gui", data);
 	}
 }
 
-// Reset GUI
+/** Reset CLI GUI */
 function reset() {
 	data = dataDefault;
+}
+
+/**
+ * Handle incoming packets
+ * @param {object} packetData
+ * @param {object} packetMeta
+ */
+function packetHandler(packetData, packetMeta) {
+	switch (packetMeta.name) {
+		case "playerlist_header": // Handle playerlist packets
+			if (data.inQueue) { // Check if in server
+				const header = JSON.parse(packetData.header).text.split("\n");
+				const position = header[5].split("l")[1];
+				const eta = header[6].split("l")[1];
+				if (position !== data.position) { // Position
+					display("position", position);
+					if (data.position <= config.queueThreshold) { // Notifications
+						notifier.sendToast("2B2T Queue Position: " + data.position);
+						updatePosition(true);
+					} else {
+						updatePosition(false);
+					}
+				}
+				if (eta !== data.eta) { // ETA
+					display("eta", eta);
+				}
+			} else {
+				var position = "In Server!";
+				var eta = "Now!";
+				if (data.position !== position) {
+					display("position", "In Server!");
+				}
+				if (data.eta !== eta) {
+					display("eta", "Now!");
+				}
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+/**
+ * Update position in CLI GUI & webhook
+ * @param {boolean} ping
+ */
+function updatePosition(ping) {
+	notifier.sendWebhook({
+		title: "2B2T Queue Position: " + data.position,
+		ping: ping,
+		footer: "ETA: " + data.eta,
+		url: config.discord.webhook.position
+	});
 }
 
 // =======
@@ -59,5 +121,6 @@ function reset() {
 module.exports = {
 	data,
 	display,
-	reset
+	reset,
+	packetHandler
 };
