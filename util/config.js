0226ad2cc20d803fd3690c79f7ac0e3867c7c3c3
Encho <38841444+Enchoseon@users.noreply.github.com>
2022-03-25 04:18:10 -0700

    Moved data object and some logic from gui.js to config.js. Renamed data object to status. Added logic for coordinatorStatus.

--- util/config.js
+++ util/config.js
@@ -11,17 +11,73 @@ const merge = require("deepmerge");
 // ===========
 
 var config = {};
+var status = {
+	"position": "CHECKING...",
+	"eta": "CHECKING...",
+	"restart": "None",
+	"mineflayer": "CHECKING...",
+	"inQueue": "false",
+	"ngrokUrl": "None",
+	"livechatRelay": "false"
+};
+var coordinatorStatus = {};
 
-// ==============
-// Process Config
-// ==============
+// ===============
+// Process Configs
+// ===============
 
 config = JSON.parse(fs.readFileSync("config.json"));
 
-// Apply overrides
 if (config.coordination.active) {
+	// Apply master config overrides
 	const masterConfig = JSON.parse(fs.readFileSync(config.coordination.masterConfigPath));
 	config = merge(config, masterConfig);
+	// Get current coordinator JSON file
+	coordinatorStatus = JSON.parse(fs.readFileSync(config.coordination.coordinatorPath));
+}
+
+// =========
+// Functions
+// =========
+
+/**
+ * Update status object. Returns whether the input being received is different from what's already stored in the object.
+ * @param {string} type
+ * @param {string} input
+ */
+function updateStatus(type, input) {
+	if (status[type] !== input.toString()) {
+		status[type] = input.toString();
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Merge GUI object into coordinator JSON
+ * @param {object} data
+ */
+function updateCoordinatorStatus(data) {
+	if (config.coordination.active) {
+		fs.readFile(config.coordination.coordinatorPath, (error, coordinatorData) => {
+			if (error && error.code == "ENOENT") { // If coordinator json doesn't exist, default to empty object
+				coordinatorData = {};
+			} else if (!error) { // Otherwise, read the coordinator JSON
+				coordinatorData = JSON.parse(coordinatorData);				
+			} else {
+				log("updateCoordinatorStatus", error, "error");
+			}
+			// Merge GUI data object with coordinatorData
+			coordinatorData[config.account.username] = data;
+			// Write to coordinator JSON and coordinatorStatus
+			coordinatorStatus = coordinatorData;
+			fs.writeFile(config.coordination.coordinatorPath, JSON.stringify(coordinatorData), (error) => {
+				if (error) {
+					log("updateCoordinatorStatus", error, "error");
+				}
+			});
+		});
+	}
 }
 
 // =======
@@ -29,5 +85,9 @@ if (config.coordination.active) {
 // =======
 
 module.exports = {
-	config
+	config,
+	status,
+	coordinatorStatus,
+	updateStatus,
+	updateCoordinatorStatus
 };
