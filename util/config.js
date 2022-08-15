844e6037d05cbd46c34a2e7c46abd498563b4f47
Encho <38841444+Enchoseon@users.noreply.github.com>
2022-08-15 14:57:32 -0700

    Config schema nested objects fix
    Joi will now construct nested objects out of default values if they weren't defined in the config.json.

--- util/config.js
+++ util/config.js
@@ -29,7 +29,7 @@ var status = { // Stores pertinent information (to-do: set up setters and getter
 // =======
 
 // Schema used to validate Minecraft usernames (between 3 and 16 characters, containing only a-z, A-Z, 0-9, and _)
-const usernameSchema = joi.string().min(3).max(16).token(); 
+const usernameSchema = joi.string().min(3).max(16).token();
 
 // Schema used to validate packet names (lowercase, consisting only of a-z and underscores)
 const packetSchema = joi.string().pattern(/^[a-z_]*$/).lowercase();
@@ -40,7 +40,7 @@ const configSchema = joi.object({
 		"username": usernameSchema,
 		"password": joi.string().empty("").default(""), // to-do: add a mojang password regex
 		"auth": joi.string().valid("microsoft", "mojang", "offline").default("microsoft")
-	}),
+	}).default(),
 	"discord": joi.object({
 		"active": joi.boolean().default(false),
 		"webhook": joi.object({
@@ -50,7 +50,7 @@ const configSchema = joi.object({
 		}),
 		"color": joi.number().integer().min(0).max(16777215).default(2123412),
 		"id": joi.string().default(0) // although this can be an number for users, it can be a string for roles!
-	}),
+	}).default(),
 	"queueThreshold": joi.number().integer().min(0).default(21),
 	"reconnectInterval": joi.number().positive().default(69),
 	"uncleanDisconnectInterval": joi.number().positive().default(420),
@@ -61,56 +61,56 @@ const configSchema = joi.object({
 			"chat": joi.boolean().default(true),
 			"bridgeClientPackets": joi.boolean().default(true),
 			"serverPackets": joi.boolean().default(true),
-		}),
+		}).default(),
 		"cutoff": joi.number().integer().positive().default(42069), // Not setting a minimum for this seems dangerous...
 		"packetFilters": joi.object({
 			"server": joi.array().items(packetSchema).default(["map", "map_chunk", "player_info", "entity_metadata", "entity_velocity", "entity_move_look", "entity_look", "update_time", "world_particles", "unload_chunk", "teams", "rel_entity_move", "entity_head_rotation", "entity_update_attributes", "block_change"]),
 			"bridgeClient": joi.array().items(packetSchema).default(["position", "look", "position_look", "arm_animation"])
-		}),
-	}),
+		}).default(),
+	}).default(),
 	"server": joi.object({
 		"host": joi.string().hostname().default("connect.2b2t.org"),
 		"version": joi.string().default("1.12.2"), // to-do: regex idea: must start with a number and contain only numbers & decimals?
 		"port": joi.number().port().default(25565)
-	}),
+	}).default(),
 	"proxy": joi.object({
 		"whitelist": joi.array().items(usernameSchema),
 		"onlineMode": joi.boolean().default(true),
 		"port": joi.number().port().default(25565)
-	}),
+	}).default(),
 	"ngrok": joi.object({
 		"active": joi.boolean().default(false),
 		"authtoken": joi.string().empty("").pattern(/[A-Za-z0-9\-\._~\+\/]+=*/).default(""), // (Bearer Token Regex) From: https://www.regextester.com/95017
 		"region": joi.string().valid("us", "eu", "au", "ap", "sa", "jp", "in").default("us"), // From: https://ngrok.com/docs/ngrok-agent/ (under "--region string")
-	}),
+	}).default(),
 	"mineflayer": joi.object({
 		"active": joi.boolean().default(true),
 		"autoQueueMainInterval": joi.number().positive().default(690), // Not setting a minimum for this seems dangerous...
 		"killAura": joi.object({
 			"interval": joi.number().positive().default(0.69),
 			"blacklist": joi.array().items(packetSchema).default(["zombie_pigman", "enderman"])
-		}),
+		}).default(),
 		"autoEat": joi.object({
 			"priority": joi.string().valid("saturation", "foodPoints", "effectiveQuality").default("saturation"), // From: https://github.com/link-discord/mineflayer-auto-eat#botautoeatoptionspriority
 			"startAt": joi.number().integer().min(0).default(19),
 			"bannedFood": joi.array().items(packetSchema).default(["rotten_flesh", "pufferfish", "chorus_fruit", "poisonous_potato", "spider_eye"])
-		}),
+		}).default(),
 		"antiAfk": joi.object({
 			"actions": joi.array().items(joi.string().valid("rotate", "walk", "jump", "jumpWalk", "swingArm", "breakBlock")).default(["rotate"]),
 			"fishing": joi.boolean().default(false),
 			"chatting": joi.boolean().default(false),
 			"chatMessages": joi.array().items(joi.string().min(1).max(256)).default(["!pt", "!queue"]), // to-do: find out any other chat limits
 			"chatInterval": joi.number().integer().positive().default(690420) // Not setting a minimum for this seems dangerous...
-		})
-	}),
+		}).default()
+	}).default(),
 	"experimental": joi.object({
 		"spoofPlayerInfo": joi.object({
 			"active": joi.boolean().default(true),
 			"texture": joi.object({ // From: https://wiki.vg/Mojang_API#UUID_to_Profile_and_Skin.2FCape
 				"value": joi.string().empty("").base64({ urlSafe: true, paddingRequired: true }).default(""),
 				"signature": joi.string().empty("").base64({ paddingRequired: true }).default(""),
-			})
-		}),
+			}).default()
+		}).default(),
 		"spoofPing": joi.object({
 			"active": joi.boolean().default(false),
 			"noResponse": joi.boolean().default(false),
@@ -118,7 +118,7 @@ const configSchema = joi.object({
 				"version": joi.object({
 					"name": joi.string().default("1.12.2"),
 					"protocol": joi.number().integer().default(340) // From: https://wiki.vg/Protocol_version_numbers
-				}),
+				}).default(),
 				"players": joi.object({
 					"max": joi.number().integer().default(20), // From: https://minecraft.fandom.com/wiki/Server.properties#Java_Edition_2 (under "max-players")
 					"online": joi.number().integer().default(0),
@@ -126,28 +126,28 @@ const configSchema = joi.object({
 						"name": usernameSchema,
 						"id": joi.string().uuid({ version: "uuidv4", separator: "-" })
 					})).default([])
-				}),
+				}).default(),
 				"description": joi.object({
 					"text": joi.string().default("A Minecraft server")
-				}),
+				}).default(),
 				"favicon": joi.string().default("undefined")
-			})
-		}),
+			}).default()
+		}).default(),
 		"disconnectIfNoController": joi.object({
 			"active": joi.boolean().default(false),
 			"delay": joi.number().min(0).default(7) // Not setting a minimum for this seems dangerous...
-		})
+		}).default()
 	}),
 	"waitForControllerBeforeConnect": joi.boolean().default(false),
 	"notify": joi.object({
 		"whenJoining": joi.boolean().default(true),
 		"whenBelowQueueThreshold": joi.boolean().default(true),
 		"whenControlling": joi.boolean().default(false)
-	}),
+	}).default(),
 	"coordination": joi.object({
 		"active": joi.boolean().default(false),
 		"path": joi.string().default("./../")
-	})
+	}).default()
 });
 
 // =======================
@@ -258,7 +258,7 @@ function getTimestamp(includeTime) {
 	var timestamp = new Date();
 	timestamp = timestamp.toLocaleString();
 	return timestamp.replace(/\//g, "-") // Replace forward-slash with hyphen
-					.replace(",", ""); // Remove comma
+		.replace(",", ""); // Remove comma
 }
 
 /**
