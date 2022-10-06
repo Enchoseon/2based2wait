c5b699eea373cbbb164f8f63424676616e86bf72
Encho <38841444+Enchoseon@users.noreply.github.com>
2022-10-05 23:41:38 -0700

    Parse chat messages with prismarine-chat
    Was already part of the dependency tree and greatly improves reliability of parsing of all chat messages across all Minecraft versions.

--- util/chatty.js
+++ util/chatty.js
@@ -8,6 +8,8 @@ const { config, status, updateStatus } = require("./config.js");
 const logger = require("./logger.js");
 const notifier = require("./notifier.js");
 
+const ChatMessage = require("prismarine-chat")(config.server.version);
+
 // =========
 // Functions
 // =========
@@ -18,21 +20,12 @@ const notifier = require("./notifier.js");
  */
 function chatPacketHandler(packetData) {
 	// Parse chat messages
-	var msgObj = JSON.parse(packetData.message);
-	var msgUsername = msgObj.text || "";
-	var msgText = "";
-	if (msgObj.extra) { // Being incredibly careful and meandering about how I do this because I didn't read the protocol documentation
-		msgObj.extra.forEach((extraObj) => {
-			if (extraObj.text) {
-				msgText += extraObj.text;
-			}
-		});
-	}
-	const msg = msgUsername + " " + msgText;
+	const msgObj = JSON.parse(packetData.message);
+	const msg = ChatMessage.fromNotch(msgObj).toString();
 
 	// Notify about server restarts (haven't been tested in a long time due to restarts being less common)
-	if (msgText && msgText.startsWith("[SERVER] Server restarting in ")) {
-		var restart = msgText.replace("[SERVER] Server restarting in ", "").replace(" ...", "");
+	if (msg && msg.startsWith("[SERVER] Server restarting in ")) {
+		var restart = msg.replace("[SERVER] Server restarting in ", "").replace(" ...", "");
 		if (updateStatus("restart", restart)) {
 			notifier.sendToast("Server Restart In: " + status.restart);
 			notifier.sendWebhook({
