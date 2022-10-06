b7a7bad0d3dc1d64a3b9e9255af60c0730f4fafc
Encho <38841444+Enchoseon@users.noreply.github.com>
2022-10-05 23:24:29 -0700

    New log filenames and reuse valid logs
    No longer will your log folder get filled with hundreds of 1 kb files with 3 lines of useless text in them.
    
    No longer will those log files have bullshit random number enumerators that make it so that files displayed in alphabetical order are totally random their last-modified order.
    
    Oh, also added the useless ability to write Gzipped log files. This is 1,000% useless and would probably just be a nuisance unless you've changed your logging filters from the defaults and opted to log everything and are hoarding gigabyte-sized logs like an absolute freak.

--- util/logger.js
+++ util/logger.js
@@ -3,6 +3,7 @@
 // =======
 
 const fs = require("fs");
+const zlib = require("zlib");
 
 const { config } = require("./config.js");
 
@@ -10,8 +11,33 @@ const { config } = require("./config.js");
 // Global Vars
 // ===========
 
-var logIndex = 1;
-const logRandomEnumerator = String(Math.random()).replace(".", "");
+var logFiles = {};
+
+// ===================
+// Initialize Logfiles
+// ===================
+
+// Create the directories and plan which logfiles to write to
+for (const category in config.log.active) {
+	if (!config.log.active[category]) { // Don't proceed if logging category is disabled in config.json
+		return;
+	}
+	const dir = createDirectory(category); // Create directory for category if it doesn't exist
+	const files = fs.readdirSync(dir, { // Get all files in the directory with the correct extension (either .log.gz or .log)
+		"withFileTypes": true
+	}).filter(f => {
+		if (config.log.compression.active) {
+			return f.name.endsWith(".log.gz");
+		} else {
+			return f.name.endsWith(".log");
+		}
+	});
+	var file = findExisting(dir, category, files); // Pick a filename (either a valid existing one or a new one)
+	if (!file) {
+		file = createFilename(category, files.length + 1);
+	}
+	logFiles[category] = dir + file; // Push file path to logFiles
+}
 
 // =========
 // Functions
@@ -24,7 +50,6 @@ const logRandomEnumerator = String(Math.random()).replace(".", "");
  * @param {string} source
  */
 function packetHandler(packetData, packetMeta, category) {
-	var filter = [];
 	if (config.log.packetFilters[category].indexOf(packetMeta.name) === -1) { // Don't log filtered packets
 		log(packetMeta.name, packetData, category + "Packets");
 	}
@@ -41,32 +66,73 @@ function log(name, data, category) {
 	if (!config.log.active[category]) {
 		return;
 	}
-	// Create log folder if it doesn't exist
+	// Create file name
+	createDirectory(category);
+	var logFile = logFiles[category];
+	// Create log message
+	const logMessage = "[" + getTimestamp() + "] [" + name + "] " + JSON.stringify(data) + "\n";
+	// Write to log (either gzipped or raw)
+	var stream = fs.createWriteStream(logFile, {
+		flags: "a"
+	});
+	if (config.log.compression.active) {
+		stream.write(zlib.gzipSync(logMessage, { // Save Gzipped
+			level: config.log.compression.level,
+			strategy: 1,
+		}));
+	} else {
+		stream.write(logMessage); // Save raw
+	}
+	stream.end();
+}
+
+/**
+ * Create a directory if it doesn't exist (in "./log/${category}/"). Also makes sure that logs from mocha tests don't contaminate normal logs.
+ * @param {string} category
+ */
+function createDirectory(category) {
+	// Choose the directory
 	var dir = "./log/" + category + "/";
-	if (typeof global.it === "function") { // Change directory to test directory if being ran by mocha.
+	if (typeof global.it === "function") { // (Change to a test directory if being ran by mocha.)
 		dir = "./test/log/" + category + "/";
 	}
+	// Create directory if it doesn't exist
 	if (!fs.existsSync(dir)) {
 		fs.mkdirSync(dir, {
 			recursive: true
 		});
 	}
-	// Create file name
-	var logFile = dir + category + "_" + getTimestamp(true) + "_" + logRandomEnumerator + "_" + logIndex + ".log";
-	// Write to log
-	var stream = fs.createWriteStream(logFile, {
-		flags: "a"
-	});
-	stream.write("[" + getTimestamp() + "] [" + name + "] " + JSON.stringify(data) + "\n");
-	stream.end();
-	// Increment filename if size is too big
-	fs.stat(logFile, (err, stats) => {
-		if (!err) { // ez error handling
-			if (stats.size >= config.log.cutoff * 1000) {
-				logIndex++;
-			}
+	return dir;
+}
+
+/**
+ * Return a filename for a log category.
+ * @param {string} category
+ * @param {number} index
+ */
+function createFilename(category, index) {
+	var filename = category + "_" + index + ".log";
+	if (config.log.compression.active) {
+		filename += ".gz";
+	}
+	return filename;
+}
+
+/**
+ * Returns a filename of the most recent and valid log to continue using (and false otherwise).
+ * @param {string} dir
+ * @param {string} category
+ * @param {string} files
+ */
+function findExisting(dir, category, files) {
+	for (var i = files.length - 1; i >= 0; i--) {
+		const file = files[i].name;
+		// Check if there's an existing log file to reuse and whether it has enough space to write to
+		if (file === createFilename(category, files.length) && fs.existsSync(dir) && fs.statSync(dir + file).size < config.log.cutoff * 1000) {
+			return file;
 		}
-	});
+	}
+	return false;
 }
 
 /**
