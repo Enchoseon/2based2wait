23e66a487e19c49690e6fece9e0a780ea32c1d27
donbendito <118032948+donbendito@users.noreply.github.com>
2022-11-25 12:38:35 -0700

    Update webserver.js

--- util/webserver.js
+++ util/webserver.js
@@ -3,28 +3,66 @@ const express = require("express");
 const app = express();
 var server = require('http').createServer(app); 
 var io = require('socket.io')(server); 
+const path = require('path');
+
 const chatLog = [""];
-const startwebUI = function startwebUI() {
-	app.use(express.static("views")); // public folder for css and images
-	app.use(express.static("public")); // public folder for css and images
-	app.set("view engine", "ejs"); // seting the engine for ejs
-	app.get('/', function (req, res){res.render('index');});
+function startStatic(){
+	app.use(express.static("views")); // public folder for css and image
+  app.use(express.static("public")); // public folder for css and images
+}
+
+function startwebUI() {
+	app.get("/", (req, res) => {
+  const reject = () => {
+    res.setHeader("www-authenticate", "Basic");
+    res.sendStatus(401);
+  };
+
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return reject();
+  }
+
+  const [username, password] = Buffer.from(
+    authorization.replace("Basic ", ""),
+    "base64"
+  )
+    .toString()
+    .split(":");
+
+  if (!(username === site.webusername && password === site.webpassword)) {
+    return reject();
+  }
+  app.use(express.static("views")); // public folder for css and image
+  app.use(express.static("public")); // public folder for css and images
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+	
 	server.listen(site.webport, function(){
 	  console.log('listening on *:' + site.webport);
 	}); 
-io.on('connection', function(client) { 
-	io.emit('updateUsername', site.username);
-	io.emit('updateController', site.controller);
-	io.emit('updateETA', site.ETA);
-	io.emit('updateQueuePosition', site.queuePlace);
-	io.emit('updateChatLog', chatLog);
+	io.on('connection', function(client) { 
+		io.emit('updateUsername', "<img hight='10%' width='10%' src='https://mc-heads.net/avatar/" + site.username + "' /> " + site.username);
+		io.emit('updateController', site.controller);
+		io.emit('updateETA', site.ETA);
+		io.emit('updateQueuePosition', site.queuePlace);
+		io.emit('updateChatLog', chatLog);
+		io.emit('updateHealth', site.playerHealth);
+		io.emit('updateHunger', site.playerHunger);
+		io.emit('updateWhitelist', site.whitelist);
+		
 	});
+
 }
-const updatewebUI = function updatewebUI() {
-	io.emit('updateUsername', site.username);
+function updatewebUI() {
+	io.emit('updateUsername', "<img hight='10%' width='10%' src='https://mc-heads.net/avatar/" + site.username + "' /> " + site.username);
 	io.emit('updateController', site.controller);
 	io.emit('updateETA', site.ETA);
 	io.emit('updateQueuePosition', site.queuePlace);
+	io.emit('updateHealth', site.playerHealth);
+	io.emit('updateHunger', site.playerHunger);
+	io.emit('updateWhitelist', site.whitelist);
 }
 function updateChat (chat){
 	chatLog.push("[" + getTimestamp() + "] " + chat);
@@ -45,11 +83,23 @@ var site = {
 	username: "None", //username for webserver
 	queuePlace: "None", //our place in queue
 	controller: "None", //controller 
+	playerHealth: "0",
+	playerHunger: "0",
+	whitelist: [""],
+	webusername: "admin",
+	webpassword: "password",
 	webport: 3000
 }
+
 module.exports = {
 	site,
 	updatewebUI,
 	startwebUI,
-	updateChat
-};
+	updateChat,
+	io: () => {
+    if (!io) {
+      throw new Error("socket is not initialized");
+    }
+    return io;
+  }
+}
